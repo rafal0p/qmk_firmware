03e8fc6fbb2ca236264a1073ced5f92915d4c5d8
Rafal Podlipny <rafal.p@fresha.com>
2022-10-10 21:36:16 +0200

    psql encourages backslash on base layer

--- keyboards/sofle/keymaps/rafal0p/keymap.c
+++ keyboards/sofle/keymaps/rafal0p/keymap.c
@@ -27,7 +27,7 @@ enum tap_dance_codes {
     DNC_1, DNC_2 ,DNC_3 , DNC_4 ,DNC_5           ,DNC_6 ,DNC_7 ,DNC_8   ,DNC_9  ,DNC_0   ,DNC_MINS,
     DNC_Q, DNC_W ,DNC_E , DNC_R ,DNC_T           ,DNC_Y ,DNC_U ,DNC_I   ,DNC_O  ,DNC_P   ,DNC_EQL ,
     DNC_A, DNC_S ,DNC_D , DNC_F ,DNC_G           ,DNC_H ,DNC_J ,DNC_K   ,DNC_L  ,DNC_SCLN,DNC_QUOT,
-    DNC_Z, DNC_X ,DNC_C , DNC_V ,DNC_B           ,DNC_N ,DNC_M ,DNC_COMM,DNC_DOT,DNC_SLSH,DNC_GRV ,
+    DNC_Z, DNC_X ,DNC_C , DNC_V ,DNC_B           ,DNC_N ,DNC_M ,DNC_COMM,DNC_DOT,DNC_SLSH,DNC_BSLS,
     DNC_SPC, DNC_ENT
 };
 
@@ -45,15 +45,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC  ,TD(DNC_1), TD(DNC_2) ,TD(DNC_3) ,TD(DNC_4) ,TD(DNC_5)  ,                     TD(DNC_6)  ,TD(DNC_7) ,TD(DNC_8)   ,TD(DNC_9)  ,TD(DNC_0)   ,TD(DNC_MINS),
   KC_TAB  ,TD(DNC_Q), TD(DNC_W) ,TD(DNC_E) ,TD(DNC_R) ,TD(DNC_T)  ,                     TD(DNC_Y)  ,TD(DNC_U) ,TD(DNC_I)   ,TD(DNC_O)  ,TD(DNC_P)   ,TD(DNC_EQL) ,
   KC_BSPC ,TD(DNC_A), TD(DNC_S) ,TD(DNC_D) ,TD(DNC_F) ,TD(DNC_G)  ,                     TD(DNC_H)  ,TD(DNC_J) ,TD(DNC_K)   ,TD(DNC_L)  ,TD(DNC_SCLN),TD(DNC_QUOT),
-  KC_ENT  ,TD(DNC_Z), TD(DNC_X) ,TD(DNC_C) ,TD(DNC_V) ,TD(DNC_B)  ,KC_MUTE   ,XXXXXXX  ,TD(DNC_N)  ,TD(DNC_M) ,TD(DNC_COMM),TD(DNC_DOT),TD(DNC_SLSH),TD(DNC_GRV) ,
+  KC_GRV  ,TD(DNC_Z), TD(DNC_X) ,TD(DNC_C) ,TD(DNC_V) ,TD(DNC_B)  ,KC_MUTE   ,XXXXXXX  ,TD(DNC_N)  ,TD(DNC_M) ,TD(DNC_COMM),TD(DNC_DOT),TD(DNC_SLSH),TD(DNC_BSLS),
                       KC_LCTRL  ,KC_LGUI   ,KC_LALT   ,TD(DNC_SPC),KC_LOWER  ,KC_RCTRL ,TD(DNC_ENT),KC_RALT   ,KC_RAISE    ,KC_RGUI                       
 ),
 
 [_LOWER] = LAYOUT(
   QK_BOOT  ,KC_F1    ,KC_F2    ,KC_F3    ,KC_F4    ,KC_F5    ,                    KC_F6    ,KC_F7    ,KC_F8    ,KC_F9    ,KC_F10   ,KC_F11   ,
   _______  ,KC_1     ,KC_2     ,KC_3     ,KC_4     ,KC_5     ,                    KC_6     ,KC_7     ,KC_8     ,KC_9     ,KC_0     ,KC_F12   ,
-  _______  ,KC_EXLM  ,KC_AT    ,KC_HASH  ,KC_DLR   ,KC_PERC  ,                    KC_CIRC  ,KC_AMPR  ,KC_ASTR  ,KC_LPRN  ,KC_RPRN  ,KC_PIPE  ,
-  _______  ,_______  ,KC_LBRC  ,KC_RBRC  ,KC_LCBR  ,KC_RCBR  ,_______  ,_______  ,_______  ,_______  ,KC_LT    ,KC_GT    ,KC_BSLS  ,_______  ,
+  _______  ,KC_EXLM  ,KC_AT    ,KC_HASH  ,KC_DLR   ,KC_PERC  ,                    KC_CIRC  ,KC_AMPR  ,KC_ASTR  ,KC_LPRN  ,KC_RPRN  ,_______  ,
+  KC_ENT   ,KC_ENT   ,KC_LBRC  ,KC_RBRC  ,KC_LCBR  ,KC_RCBR  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______  ,
                       _______  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______  ,_______                       
 ),
 
@@ -359,7 +359,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [DNC_COMM] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_finished, dance_reset, KC_COMM),
     [DNC_DOT] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_finished, dance_reset, KC_DOT),
     [DNC_SLSH] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_finished, dance_reset, KC_SLSH),
-    [DNC_GRV] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_finished, dance_reset, KC_GRV),
+    [DNC_BSLS] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_finished, dance_reset, KC_BSLS),
     [DNC_SPC] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_spc_finished, dance_spc_reset, KC_SPC),
     [DNC_ENT] = ACTION_TAP_DANCE_FN_ADVANCED_USER(on_dance, dance_ent_finished, dance_ent_reset, KC_ENT),
 };
